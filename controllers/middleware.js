1018263f434ef778e0f0717d6d77caa9a8ba76b5
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-08-07 14:31:36 +0200

    add route

--- controllers/middleware.js
+++ controllers/middleware.js
@@ -4,6 +4,9 @@ const express = require('express')
 const exphbs = require('express-handlebars');
 const UserModel = require("../models/user");
 
+// import controller user to access functions like addUser
+const User = require('./user');
+
 /* Express routing */
 exports.run = async () => {
 
@@ -13,6 +16,8 @@ exports.run = async () => {
     Use an Express Template https://expressjs.com/en/guide/using-template-engines.html 
     Handlebars View Engine https://github.com/ericf/express-handlebars 
     */
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
     app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
     app.set('view engine', 'hbs');
     app.set('views', path.join(__dirname, '../views'));
@@ -50,6 +55,8 @@ exports.run = async () => {
     app.get('/users/add', async (req, res) => {
         res.render('add', { title: "Add User" })
     })
+
+    app.post('/users/add', User.addUser);
 
     console.log(`View user data on http://localhost:3000`);
     await app.listen(3000)
